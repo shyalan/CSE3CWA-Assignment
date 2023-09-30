a1924b2dbf26cad924d2344c83ced3b173c31b5e
Shyalan Ramesh <21298015@students.latrobe.edu.au>
2023-09-30 21:27:43 +1000

    Initial Commit

---
+++ api/controllers/contact.controller.js
@@ -0,0 +1,29 @@
+const db = require("../models");
+const Contacts = db.contacts;
+const Phones = db.phones;
+const Op = db.Sequelize.Op;
+
+// Create contact
+exports.create = (req, res) => {
+    
+};
+
+// Get all contacts
+exports.findAll = (req, res) => {
+    
+};
+
+// Get one contact by id
+exports.findOne = (req, res) => {
+  
+};
+
+// Update one contact by id
+exports.update = (req, res) => {
+    
+};
+
+// Delete one contact by id
+exports.delete = (req, res) => {
+    
+};
